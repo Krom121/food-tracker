0b32c764c4a421e24771561c95b2c49a64b907f7
stephen <stephenmacgregor9@gmail.com>
2020-02-03 09:10:24 +0000

    added main routes

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return None
+
+@app.route('/view')
+def view():
+    return None
+
+@app.route('/add_food')
+def add_food():
+    return None
 
 
 
