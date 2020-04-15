31055d218c6cc04d2dd2d4d5eb8851a9dd62a1f0
stephen <stephenmacgregor9@gmail.com>
2020-04-15 18:23:32 +0100

    database connection

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+from flask import Flask, render_template, request, g
+import sqlite3
 
 app = Flask(__name__)
 
+def connect_db():
+    # Connect to database
+    sql = sqlite3.connect('\C:\sites\cal_tracker\food_log.db')
+    # return the results as a dictionaries instead of tuples
+    sql.row_factory = sqlite3.Row
+    return sql
 
+def get_db():
+    # check to see if sqlite3 exsits
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    # close db at end of each request
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
